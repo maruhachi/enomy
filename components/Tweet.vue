5c4a9d3e8d2a7d03eb9d00b3254acec00ae11e9a
maruhachi <konchan1129@gmail.com>
2019-01-03 04:48:13 +0900

    create new page for display tweet contents.

---
+++ components/Tweet.vue
@@ -0,0 +1,22 @@
+<template>
+  <blockquote class="twitter-tweet" data-lang="ja">
+    <p lang="ja" dir="ltr">
+        描き始めようちか、今年もようちかようちかしていきたい所存(｀･ω･´)ゞ
+         <a href="https://t.co/yKKONenCS5">pic.twitter.com/yKKONenCS5</a>
+    </p>&mdash;
+    にぎりめし🍊1日目【東Ｖ-07a】 (@onigirinigirime) 
+    <a href="https://twitter.com/onigirinigirime/status/1080505135510343680?ref_src=twsrc%5Etfw">2019年1月2日</a>
+  </blockquote>
+
+</template>
+
+<script>
+//   <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
